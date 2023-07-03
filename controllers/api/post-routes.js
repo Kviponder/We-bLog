e6d50b66a8843abb2c85fde67871a2b0e71fa10b
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-07-03 16:18:04 -0400

    Added post and put routes in post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,1 +1,43 @@
-const router = require('express').Router();
+//CRUD for posts
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+// POST /api/posts
+router.post("/", withAuth, async (req, res) => {
+  const body = req.body;
+  try {
+    const newPost = await Post.create({
+      ...body,
+      user_id: req.session.user_id,
+    });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log("Error in post-routes.js post method", err);
+  }
+});
+
+// PUT /api/posts/1
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (affectedRows > 0) {
+            res.status(200).end();
+            console.log("Post updated");
+        }
+        else {
+            res.status(404).end();
+            console.log("Post not found");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+        console.log("Error in post-routes.js Put method", err);
+    }
+});
+
+
