5b029994655dc35b0f23080d62c6d545c48c4a08
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-07-03 12:16:53 -0400

    Added express server

--- server.js
+++ server.js
@@ -1,44 +1,54 @@
+//Require dependencies
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
+//Initialize the server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//This establishes the session and connects it to our Sequelize database
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
+//This will be used to initialize the session
 app.use(session(sess));
 
+//This will be used to initialize the handlebars.js template engine
 const hbs = exphbs.create({ helpers });
 
+//This will be used to set Handlebars.js as the default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//This will be used to parse incoming JSON data and set the public folder as a static folder
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+//Require the routes folder
+app.use(require('./controllers'));
 
+//This will be used to start the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
-});
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+}
+);
