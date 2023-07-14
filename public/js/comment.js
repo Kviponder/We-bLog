08e64bd9407e3ac5eaf32e507287e640a9c43f10
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-07-14 17:18:26 -0400

    Added some comment logic, comment body reads in console, but isnt saving, or based on that isnt presenting under the post

---
+++ public/js/comment.js
@@ -0,0 +1,23 @@
+const commentHandler = async function (event) {
+  event.preventDefault();
+
+  const post_id = document.querySelector('input[name="post-id"]').value;
+  const body = document.querySelector('textarea[name="comment-body"]').value;
+  console.log(body);
+  if (body) {
+    await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        body,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // document.location.reload();
+  }
+};
+document
+  .querySelector("#comment-form")
+  .addEventListener("submit", commentHandler);
